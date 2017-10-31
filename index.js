16faba5b2119d3027f53a680cf7f24536bbe03ce
Gilgamech <Gilgamech@Gilgamech.com>
2017-10-30 19:47:59 -0700

    Adding in some old stuff with the new.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var hellobot = require('./hellobot');
+var dicebot = require('./dicebot');
+
 var events = require('./lib/clients/events');
 var retryPolicies = require('./lib/clients/retry-policies');
+
+var app = express();
+var port = process.env.PORT || 3000;
 
 module.exports = {
   WebClient: require('./lib/clients/web/client'),
@@ -15,3 +23,29 @@ module.exports = {
   RTM_MESSAGE_SUBTYPES: events.RTM_MESSAGE_SUBTYPES,
   RETRY_POLICIES: retryPolicies
 };
+
+
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// test route
+app.get('/', function (req, res) { res.status(200).send('Hello world!') });
+
+// hellobot
+app.post('/hello', hellobot);
+
+// dicebot
+app.post('/roll', dicebot);
+
+
+// basic error handler
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(400).send(err.message);
+});
+
+
+app.listen(port, function () {
+  console.log('Slack bot listening on port ' + port);
+});
